a0b5882f75ea0839c8e88eb7c400612d2caee6fa
Shre <henrystone123@gmail.com>
2023-11-02 11:16:16 -0700

    a

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/planet/PlanetRepository.java
@@ -0,0 +1,17 @@
+package com.nighthawk.spring_portfolio.mvc.planet;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class PlanetRepository {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Planet> getPlanetById(@PathVariable String id) {
+        String description = PlanetDescriptionStorage.getDescription(id);
+        if (description != null) {
+            return ResponseEntity.ok(new Planet(id, description)); // Assuming you have a Planet constructor
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
