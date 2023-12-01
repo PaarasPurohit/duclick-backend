3cd85c5980b140905c00c3dd52b8736fb53338b6
PaarasPurohit <pmpurohit2017@outlook.com>
2023-12-01 09:25:14 -0800

    contribution of bubble sort

--- src/main/java/com/nighthawk/spring_portfolio/mvc/sort/BubbleSort.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/sort/BubbleSort.java
@@ -2,11 +2,27 @@ package com.nighthawk.spring_portfolio.mvc.sort;
 
 import java.util.ArrayList;
 
-public abstract class BubbleSort extends Sorting{
+public abstract class BubbleSort extends Sorting {
 
     public BubbleSort(ArrayList<Integer> toSort) {
         super(toSort);
-        //TODO Auto-generated constructor stub
     }
-    
+
+    public void sort() {
+        int n = toSort.size();
+        boolean swapped;
+
+        do {
+            swapped = false;
+            for (int i = 1; i < n; i++) {
+                if (toSort.get(i - 1) > toSort.get(i)) {
+                    // Swap elements if they are in the wrong order
+                    int temp = toSort.get(i - 1);
+                    toSort.set(i - 1, toSort.get(i));
+                    toSort.set(i, temp);
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+    }
 }
