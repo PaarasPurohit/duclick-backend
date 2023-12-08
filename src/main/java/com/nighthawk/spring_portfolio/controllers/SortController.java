32e670fcff070f658cddcb18aeecdd523ddc1e82
tanayp327 <tanaypatel327@gmail.com>
2023-12-08 11:26:18 -0800

    shawty bad with the skechers

--- src/main/java/com/nighthawk/spring_portfolio/controllers/SortController.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/SortController.java
@@ -16,41 +16,13 @@ import java.util.ArrayList;
 @RequestMapping("/api/sorting")
 public class SortController {
 
-<<<<<<< HEAD
-    @GetMapping("/merge")
-    public long mergeSort(@RequestParam(name = "list") ArrayList<Integer> toSort) {
-        MergeSort mergeSort = new MergeSort(toSort);
-        long executionTime = mergeSort.doSort();
-=======
     @GetMapping("/merge") // Merge sort get (need to check if arraylist allowed)
     // @RequestParam handles variables binding to frontend, defaults, etc
     public double mergeSort(@RequestParam(name="toSort") ArrayList<Integer> toSort) {
         MergeSort mergeSort = new MergeSort(toSort);    
         double executionTime = mergeSort.doSort(); 
->>>>>>> 7325add7b88213f7546aac8d630576ab9ec7c64c
         return executionTime;
     }
-<<<<<<< HEAD
-
-    @GetMapping("/insertion")
-    public long insertionSort(@RequestParam(name = "list") ArrayList<Integer> toSort) {
-        InsertionSort insertionSort = new InsertionSort(toSort);
-        long executionTime = insertionSort.doSort();
-        return executionTime;
-    }
-
-    @GetMapping("/bogo")
-    public long bogoSort(@RequestParam(name = "list") ArrayList<Integer> toSort) {
-        BogoSort bogoSort = new BogoSort(toSort);
-        long executionTime = bogoSort.doSort();
-        return executionTime;
-    }
-
-    @GetMapping("/bubble")
-    public long bubbleSort(@RequestParam(name = "list") ArrayList<Integer> toSort) {
-        BubbleSort bubbleSort = new BubbleSort(toSort);
-        long executionTime = bubbleSort.doSort();
-=======
     @GetMapping("/insertion") // Insertion Sort @getmapping method
     public double insertionSort(@RequestParam(name="toSort") ArrayList<Integer> toSort) {
         InsertionSort insertionSort = new InsertionSort(toSort);    
@@ -69,7 +41,6 @@ public class SortController {
     public double bubbleSort(@RequestParam(name="toSort") ArrayList<Integer> toSort) {
         BubbleSort bubbleSort = new BubbleSort(toSort);    
         double executionTime = bubbleSort.doSort();       
->>>>>>> 7325add7b88213f7546aac8d630576ab9ec7c64c
         return executionTime;
     }
 
