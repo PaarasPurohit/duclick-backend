6b670c07f3a8cd31b4b32a622988b22580be4425
PaarasPurohit <pmpurohit2017@outlook.com>
2023-12-07 20:39:54 -0800

    I'm stumped.

--- src/main/java/com/nighthawk/spring_portfolio/controllers/SortController.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/SortController.java
@@ -16,35 +16,33 @@ import java.util.ArrayList;
 @RequestMapping("/api/sorting")
 public class SortController {
 
-    @GetMapping("/merge") // Merge sort get (need to check if arraylist allowed)
-    // @RequestParam handles variables binding to frontend, defaults, etc
-    public long mergeSort(@RequestParam(name="toSort") ArrayList<Integer> toSort) {
-        MergeSort mergeSort = new MergeSort(toSort);    
-        long executionTime = mergeSort.doSort(); 
-        return executionTime;
-
-    }
-    @GetMapping("/insertion") // Insertion Sort @getmapping method
-    public long insertionSort(@RequestParam(name="toSort") ArrayList<Integer> toSort) {
-        InsertionSort insertionSort = new InsertionSort(toSort);    
-        long executionTime = insertionSort.doSort();       
+    @GetMapping("/merge")
+    public long mergeSort(@RequestParam(name = "list") ArrayList<Integer> toSort) {
+        MergeSort mergeSort = new MergeSort(toSort);
+        long executionTime = mergeSort.doSort();
         return executionTime;
     }
 
-    @GetMapping("/bogo") // BogoSort @getmapping method
-    public long bogoSort(@RequestParam(name="toSort") ArrayList<Integer> toSort) {
-        BogoSort bogoSort = new BogoSort(toSort);    
-        long executionTime = bogoSort.doSort();       
+    @GetMapping("/insertion")
+    public long insertionSort(@RequestParam(name = "list") ArrayList<Integer> toSort) {
+        InsertionSort insertionSort = new InsertionSort(toSort);
+        long executionTime = insertionSort.doSort();
         return executionTime;
     }
 
-    @GetMapping("/bubble") // Bubblesort Get mapping method
-    public long bubbleSort(@RequestParam(name="toSort") ArrayList<Integer> toSort) {
-        BubbleSort bubbleSort = new BubbleSort(toSort);    
-        long executionTime = bubbleSort.doSort();       
+    @GetMapping("/bogo")
+    public long bogoSort(@RequestParam(name = "list") ArrayList<Integer> toSort) {
+        BogoSort bogoSort = new BogoSort(toSort);
+        long executionTime = bogoSort.doSort();
+        return executionTime;
+    }
+
+    @GetMapping("/bubble")
+    public long bubbleSort(@RequestParam(name = "list") ArrayList<Integer> toSort) {
+        BubbleSort bubbleSort = new BubbleSort(toSort);
+        long executionTime = bubbleSort.doSort();
         return executionTime;
     }
 
     // TODO: Test Methods
-
 }
